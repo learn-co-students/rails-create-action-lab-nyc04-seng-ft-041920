b0550772d39f7dedf86431782bc9d9a1eeeb152a
jinnic <yoohyojin@gmail.com>
2020-06-06 15:41:39 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,8 +15,8 @@ class StudentsController < ApplicationController
   def create
     # byebug
     @student = Student.new
-    @student.first_name= params[:student][:first_name]
-    @student.last_name = params[:student][:last_name]
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
     @student.save
        
     redirect_to student_path(@student)
